c9273950243ec2b7eb2cbbf4a3813d586cba0f3d
rhin0JS <rhinojs8@gmail.com>
2023-04-16 15:52:58 -0700

    finished

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
 const container = document.getElementById('container');
 const clearButton = document.getElementById('clear');
-const color = document.getElementById('color');
+const color = document.getElementById('colorChoice');
 const slider = document.getElementById('myRange');
 const output = document.getElementById('gridValue');
 
 let grid = 16;
+let selectedColor = color.value;
+let isDrawing = false;
 
 output.innerHTML = slider.value;
-// slider.oninput = function () {
-//   output.innerHTML = this.value;
-//   grid = Number(this.value);
-// };
 
 const sliderInput = function () {
   output.innerHTML = this.value;
-  // grid = Number(this.value);
   gridContainer(Number(this.value));
 };
-
-// slider.addEventListener('input', sliderInput);
 
 const gridContainer = function (grid) {
   for (let i = 0; i < grid * grid; i++) {
@@ -33,14 +28,47 @@ const gridContainer = function (grid) {
 const clearGrid = function () {
   const rmvSquares = document.querySelectorAll('.square');
   rmvSquares.forEach(square => square.remove());
+  gridContainer(grid);
 };
 
+// etch function controls the grid,
 const etch = function (grid) {
   gridContainer(grid);
   slider.addEventListener('input', clearGrid);
   slider.addEventListener('input', sliderInput);
 };
 
+//runs page
 etch(grid);
 
 clearButton.addEventListener('click', clearGrid);
+
+// chooses color value and stores it
+color.addEventListener('input', function () {
+  selectedColor = color.value;
+});
+
+//logic for when to be coloring
+container.addEventListener('mousedown', function () {
+  isDrawing = true;
+});
+
+container.addEventListener('mouseup', function () {
+  isDrawing = false;
+});
+
+container.addEventListener('mouseleave', function () {
+  isDrawing = false;
+});
+
+container.addEventListener('mousemove', function (e) {
+  if (isDrawing) {
+    e.target.style.backgroundColor = selectedColor;
+  }
+});
+
+container.addEventListener('click', function (e) {
+  if (!isDrawing) {
+    e.target.style.backgroundColor = selectedColor;
+  }
+});
